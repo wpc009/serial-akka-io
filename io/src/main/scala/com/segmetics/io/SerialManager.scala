1b5314551acc79ec8ab5ed32af8292442f2e1517
Wysa <wpc009@gmail.com>
2018-03-15 20:19:31 +0800

    fix ZAPDA-22 close serial port on failed open attempt.

--- io/src/main/scala/com/segmetics/io/SerialManager.scala
+++ io/src/main/scala/com/segmetics/io/SerialManager.scala
@@ -2,12 +2,13 @@ package com.segmetics.io
 
 import akka.actor.{Actor, ActorLogging}
 import com.segmetics.io.Serial._
-import purejavacomm.{CommPortIdentifier, PureJavaSerialPort, SerialPort}
+import purejavacomm._
 
 import scala.collection.mutable.ArrayBuffer
 import com.segmetics.io.Serial.{Open, PureOpen}
 import com.segmetics.io.handler.HandlerAdapter
 
+import scala.util.control.NonFatal
 import scala.util.{Failure, Success, Try}
 
 /**
@@ -15,37 +16,34 @@ import scala.util.{Failure, Success, Try}
   */
 private[io] class SerialManager extends Actor with ActorLogging {
 
-  def openSerial(c: PureOpen) = {
-    Try {
-      c match {
-        case PureOpen(port, baudRate, data, par, stop, fc, timeout, _) =>
-          val id = CommPortIdentifier.getPortIdentifier(port)
-          id.open(context.self.toString, 2000) match {
-            case sp: PureJavaSerialPort =>
-              log.debug("open port {} succeed, settings params", port)
-              if (baudRate.nonEmpty && data.nonEmpty && stop.nonEmpty && par.nonEmpty) {
-                sp.setSerialPortParams(baudRate.get, data.get, stop.get, par.get)
-              }
-              // sp.setSerialPortParams(baudRate, data, stop, par)
-              sp.notifyOnBreakInterrupt(true)
-              val receiveTimeout = timeout.getOrElse(50)
-              sp.enableReceiveTimeout(receiveTimeout)
-              //            sp.enableReceiveFraming(50)
-              if (fc.nonEmpty) {
-                sp.setFlowControlMode(fc.get)
-              }
-              sp
-            case _ => throw new RuntimeException(s"$port is not a SerialPort.")
-          }
+  def openSerial(c: PureOpen): Unit = Try(CommPortIdentifier.getPortIdentifier(c.port)).map(id => {
+    id.open(context.self.toString(), 2000)
+  }).flatMap(comm => {
+    try {
+      comm match {
+        case sp: SerialPort =>
+          log.debug("open port {} succeed, settings params", c.port)
+          sp.setSerialPortParams(c.baudRate.getOrElse(9600), c.dataBits.getOrElse(SerialPort.DATABITS_8), c.stopBits.getOrElse(SerialPort.STOPBITS_1), c.parity.getOrElse(SerialPort.PARITY_NONE))
+          sp.notifyOnBreakInterrupt(true)
+          sp.enableReceiveTimeout(c.timeout.getOrElse(50))
+          sp.setFlowControlMode(c.flowControl.getOrElse(SerialPort.FLOWCONTROL_NONE))
+          Success(sp)
+        case _ =>
+          Failure(new purejavacomm.NoSuchPortException())
       }
-    } match {
-      case Success(serialPort) =>
-        val operator = context.actorOf(SerialOperator.props(serialPort, sender(), c.handler))
-        sender ! Opened(operator, c.port)
-      case Failure(error) =>
-        sender ! CommandFailed(c, error)
+    } catch {
+      case NonFatal(e) =>
+        comm.close()
+        Failure(e)
     }
+  }) match {
+    case Success(serialPort: SerialPort) =>
+      val operator = context.actorOf(SerialOperator.props(serialPort, sender(), c.handler))
+      sender ! Opened(operator, c.port)
+    case Failure(error) =>
+      sender ! CommandFailed(c, error)
   }
+
 
   def receive = {
     case ListPorts =>
@@ -58,7 +56,9 @@ private[io] class SerialManager extends Actor with ActorLogging {
     case c: PureOpen =>
       openSerial(c)
     case c@Open(port, baudRate, dataBits, parity, stopBits, flowControl, timeout) =>
+
       import purejavacomm.SerialPort._
+
       val data = dataBits map {
         case DataBits5 => DATABITS_5
         case DataBits6 => DATABITS_6
